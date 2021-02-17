96889d3e17dcb62d6ed52505a97b6093c707e413
Toni Einecker <toni.einecker@gmail.com>
2021-02-17 09:55:42 +0100

    Moved file access from ScoreManager to new ScoreDataController

--- Wortfinder/ScoreManager.cs
+++ Wortfinder/ScoreManager.cs
@@ -11,18 +11,13 @@ namespace Wortfinder
 	public class ScoreManager
 	{
         private List<Score> scores;
-        private readonly byte[] key = { 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16 };
-        private readonly string highscoreLocation = ".\\Highscores.bin";
-        private readonly Aes aes;
         private readonly IScoreWindowController scoreWindowController;
+        private readonly ScoreDataController scoreDataController;
 
         public ScoreManager(IFactory factory)
 		{
             scoreWindowController = factory.GetScoreWindowController();
-            aes = Aes.Create();
-            aes.Key = key;
-            //aes.IV = yourByteArrayIV;
-            scores = new List<Score>();
+            scoreDataController = new ScoreDataController();
             LoadScores();
         }
 
@@ -39,66 +34,14 @@ namespace Wortfinder
             return scoreList;
         }
 
-		
-
         private void LoadScores()
 		{
-			if (File.Exists(highscoreLocation)) 
-            { 
-                try
-                {
-                    //Create a file stream.
-                    using FileStream myStream = new FileStream(highscoreLocation, FileMode.Open);
-
-                    //Reads IV value from beginning of the file.
-                    byte[] iv = new byte[aes.IV.Length];
-                    myStream.Read(iv, 0, iv.Length);
-
-                    //Create a CryptoStream, pass it the file stream, and decrypt
-                    //it with the Aes class using the key and IV.
-                    using CryptoStream cryptStream = new CryptoStream(
-                       myStream,
-                       aes.CreateDecryptor(key, iv),
-                       CryptoStreamMode.Read);
-
-                    //Read the stream.
-                    BinaryFormatter bf = new BinaryFormatter();
-                    //using StreamReader sReader = new StreamReader(cryptStream);
-
-                    scores = (List<Score>)bf.Deserialize(cryptStream);
-                }
-                catch
-                {
-                    Console.WriteLine("The decryption failed.");
-                    throw;
-                }
-            }
+            scores = scoreDataController.LoadScores();
         }
 
 		private void SaveScores()
 		{
-            try
-            {
-				//Create a file stream
-				using FileStream myStream = new FileStream(highscoreLocation, FileMode.Create);
-				byte[] iv = aes.IV;
-				myStream.Write(iv, 0, iv.Length);
-
-				//Create a CryptoStream, pass it the FileStream, and encrypt
-				//it with the Aes class.  
-				using CryptoStream cryptStream = new CryptoStream(
-					myStream,
-					aes.CreateEncryptor(),
-					CryptoStreamMode.Write);
-
-				new BinaryFormatter().Serialize(cryptStream, scores);
-			}
-            catch
-            {
-                //Inform the user that an exception was raised.  
-                Console.WriteLine("The encryption failed.");
-                throw;
-            }
+            scoreDataController.SaveScores(scores);
         }
 
         internal void NewScore(int score, int size, int time)
